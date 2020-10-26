b3c683f2c564dc56b238eba134494d4e6d5e90cc
Kou <1176508447@qq.com>
2020-10-26 19:23:08 +0800

    V0.8
    缩放，中心，移动实现了？没有用接口实现，每加传感器控制，缩放中心还有很大问题？先传一下吧，还要写作业。。

--- app/src/main/java/com/example/one_handed_performance_test/MainActivity.kt
+++ app/src/main/java/com/example/one_handed_performance_test/MainActivity.kt
@@ -16,31 +16,35 @@ class MainActivity : AppCompatActivity(){
         setContentView(R.layout.activity_main)
 
         enlarge.setOnClickListener{
-            val layout: RelativeLayout = findViewById(R.id.`object`)//按钮所在布局的放缩
-            val params: ViewGroup.LayoutParams = layout.layoutParams
-            params.height = (params.height*1.1).toInt()
-            params.width =  (params.width*1.1).toInt()
-            layout.layoutParams = params
+            val layout: RelativeLayout = findViewById(R.id.`object`)//按钮所在布局的放缩，布局上的按钮随之放缩
 
-            val layout1: LinearLayout = findViewById(R.id.buttons)//五个按钮的放缩
-            val params1: ViewGroup.LayoutParams = layout1.layoutParams
-            params1.height = (params1.height*1.1).toInt()
-            params1.width =  (params1.width*1.1).toInt()
-            layout1.layoutParams = params1
+            layout.pivotX=100F;//放缩中心，(0,0)为。。老实说我也不太懂它的单位与方式。
+            layout.pivotY=100F;
+
+            layout.scaleX=(layout.scaleX)*1.1F;//大小
+            layout.scaleY=(layout.scaleY)*1.1F;
         }
 
         narrow.setOnClickListener{
-            val layout2: RelativeLayout = findViewById(R.id.`object`)//同上
-            val params2: ViewGroup.LayoutParams = layout2.layoutParams
-            params2.height = (params2.height*0.9).toInt()
-            params2.width =  (params2.width*0.9).toInt()
-            layout2.layoutParams = params2
+            val layout: RelativeLayout = findViewById(R.id.`object`)//同上
 
-            val layout3: LinearLayout = findViewById(R.id.buttons)
-            val params3: ViewGroup.LayoutParams = layout3.layoutParams
-            params3.height = (params3.height*0.9).toInt()
-            params3.width =  (params3.width*0.9).toInt()
-            layout3.layoutParams = params3
+            layout.pivotX= 300F;
+            layout.pivotY= 300F;
+
+            layout.scaleX=(layout.scaleX)*0.9F;
+            layout.scaleY=(layout.scaleY)*0.9F;
+        }
+
+        right.setOnClickListener{
+            val layout: RelativeLayout = findViewById(R.id.`object`)
+
+            layout.translationX=layout.translationX+100;//针对图像左边，单位pixel
+        }
+
+        down.setOnClickListener{
+            val layout: RelativeLayout = findViewById(R.id.`object`)
+
+            layout.translationY=layout.translationY+100;//针对top(不知道是那个top)，单位pixel
         }
     }
 
