b85c0c3c423f8e581160b26c061f3eab2eb4913d
Kou <1176508447@qq.com>
2020-10-28 20:02:14 +0800

    V0.9
    缩放中心bug解决

--- app/src/main/java/com/example/one_handed_performance_test/MainActivity.kt
+++ app/src/main/java/com/example/one_handed_performance_test/MainActivity.kt
@@ -16,9 +16,6 @@ class MainActivity : AppCompatActivity(){
         enlarge.setOnClickListener{
             val layout: RelativeLayout = findViewById(R.id.`object`)//按钮所在布局的放缩，布局上的按钮随之放缩
 
-            layout.pivotX=100F//放缩中心，(0,0)为。。老实说我也不太懂它的单位与方式。
-            layout.pivotY=100F
-
             layout.scaleX=(layout.scaleX)*1.1F//大小
             layout.scaleY=(layout.scaleY)*1.1F
         }
@@ -26,14 +23,11 @@ class MainActivity : AppCompatActivity(){
         narrow.setOnClickListener{
             val layout: RelativeLayout = findViewById(R.id.`object`)//同上
 
-            layout.pivotX= 300F
-            layout.pivotY= 300F
-
             layout.scaleX=(layout.scaleX)*0.9F
             layout.scaleY=(layout.scaleY)*0.9F
         }
 
-        right.setOnClickListener{
+        /*right.setOnClickListener{
             val layout: RelativeLayout = findViewById(R.id.`object`)
 
             layout.translationX=layout.translationX+100//针对图像左边，单位pixel
@@ -43,6 +37,30 @@ class MainActivity : AppCompatActivity(){
             val layout: RelativeLayout = findViewById(R.id.`object`)
 
             layout.translationY=layout.translationY+100//针对top(不知道是那个top)，单位pixel
+        }*/
+
+        leftUp.setOnClickListener{
+            val layout: RelativeLayout = findViewById(R.id.`object`)
+
+            val newX= 0f//此处设置缩放中心
+            val newY= 0f
+
+            layout.translationX = layout.translationX + (layout.pivotX - newX) * (1 - layout.scaleX)
+            layout.translationY = layout.translationY + (layout.pivotY - newY) * (1 - layout.scaleY)
+            layout.pivotX=newX
+            layout.pivotY=newY
+        }
+
+        rightDown.setOnClickListener{
+            val layout: RelativeLayout = findViewById(R.id.`object`)
+
+            val newX= layout.width.toFloat()
+            val newY= layout.height.toFloat()
+
+            layout.translationX = layout.translationX + (layout.pivotX - newX) * (1 - layout.scaleX)
+            layout.translationY = layout.translationY + (layout.pivotY - newY) * (1 - layout.scaleY)
+            layout.pivotX=newX
+            layout.pivotY=newY
         }
     }
 
