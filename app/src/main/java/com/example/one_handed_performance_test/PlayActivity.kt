68a16c926ffbd2951c7bf82a4e28978f792a3075
Xuyq <xuyq18@lzu.edu.cn>
2020-10-31 23:53:06 +0800

    nm

--- app/src/main/java/com/example/one_handed_performance_test/PlayActivity.kt
+++ app/src/main/java/com/example/one_handed_performance_test/PlayActivity.kt
@@ -10,36 +10,10 @@ import kotlinx.android.synthetic.main.button_array.*
 import kotlin.concurrent.thread
 
 class PlayActivity : AppCompatActivity() {
-//    private var flag = true
-//    private val refresh = 1
-//    val handler = object : Handler() {
-//        override fun handleMessage(msg: Message) {
-//            when (msg.what) {
-//                refresh -> {
-//                    changeableLayout.refresh()
-//                    flag = false
-//                }
-//            }
-//        }
-//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play)
 
-
-        thread {
-            while(true) {
-                while (true) {
-                    if (changeableLayout.isClickTheTarget()) {
-                        break
-                    }
-                }
-                val msg = Message()
-                msg.what = refresh
-                handler.sendMessage(msg)
-                while (flag) {}
-            }
-        }
     }
 }
